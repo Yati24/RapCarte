8c4ceae91beaed5f3a6e44fd3efdbdf8f59df996
Yati24 <cabayetom@yahoo.fr>
2025-08-20 15:40:46 +0200

    map and basic interface added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, redirect, render_template, request, session
 from extensions import db, migrate, login_manager, bcrypt
 from config import Config
 
+from models import User, Rapper, Vote
 
 
 # ----- INIT APP -----
@@ -11,7 +12,10 @@ app.config.from_object("config.Config")
 # ----- EXTENSIONS -----
 db.init_app(app)
 migrate.init_app(app, db)
-login_manager.init_app(app)
+
+# pas activé pour éviter les erreurs, pas encore de login
+# login_manager.init_app(app)
+
 bcrypt.init_app(app)
 
 # ----- MODELS -----
@@ -20,7 +24,28 @@ from models import User, Rapper, Vote
 # ----- ROUTES -----
 @app.route("/")
 def index():
-    return "Rap Carte ✅"
+    rappers = Rapper.query.all()
+    return render_template("map.html", rappers=rappers)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/contribute")
+def contribute():
+    return render_template("contribute.html", title="Contribute")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Login")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html", title="Privacy Policy")
 
 
 # ----- RUN -----
